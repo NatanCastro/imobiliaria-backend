0f3e2b04da3d318526e4768492ed5cc97c7c59a9
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2023-07-13 19:26:49 -0300

    refactor(app.module.ts): remove unused EncryptModule import to improve code cleanliness and organization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { PrismaModule } from './prisma/prisma.module'
-import { EncryptModule } from './encrypt/encrypt.module'
 import { RealStateModule } from './real-state/real-state.module'
 import { CloudinaryModule } from './cloudinary/cloudinary.module'
 
 @Module({
-  imports: [PrismaModule, EncryptModule, RealStateModule, CloudinaryModule, ConfigModule.forRoot({ isGlobal: true })]
+  imports: [PrismaModule, RealStateModule, CloudinaryModule, ConfigModule.forRoot({ isGlobal: true })]
 })
 export class AppModule {}
