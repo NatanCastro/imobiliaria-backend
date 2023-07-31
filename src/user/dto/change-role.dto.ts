0a9f405170f78d835a5dd444bd5d7e5d4217b66e
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2023-07-31 14:29:46 -0300

    change changeRole route from GET to POST and update parameter handling to use route parameter instead of request body for userId

--- src/user/dto/change-role.dto.ts
+++ src/user/dto/change-role.dto.ts
@@ -2,7 +2,5 @@ import { IsString } from 'class-validator'
 
 export class ChangeRoleDto {
   @IsString()
-  userId: string
-  @IsString()
   role: string
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,12 @@
-import { Body, Controller, Get } from '@nestjs/common'
+import { Body, Controller, Param, Post } from '@nestjs/common'
 import { ChangeRoleDto } from './dto/change-role.dto'
 import { ClerkService } from 'src/clerk/clerk.service'
 
 @Controller('user')
 export class UserController {
   constructor(private readonly clerkService: ClerkService) {}
-  @Get('change-role')
-  changeRole(@Body() { userId, role }: ChangeRoleDto) {
-    this.clerkService.updateUserRole(userId, role)
+  @Post(':id/change-role')
+  changeRole(@Param('id') id: string, @Body() { role }: ChangeRoleDto) {
+    this.clerkService.updateUserRole(id, role)
   }
 }
