8a1c3ebfaa1b5de161af667a06810abc757a1781
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2023-07-31 15:20:24 -0300

    modify enableCors to accept multiple origins by splitting FRONTEND_URL environment variable

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as morgan from 'morgan'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors({ origin: process.env.FRONTEND_URL })
+  app.enableCors({ origin: process.env.FRONTEND_URL.split(',') })
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
   app.use(morgan('dev'))
   await app.listen(process.env.PORT || 4242, '0.0.0.0')
